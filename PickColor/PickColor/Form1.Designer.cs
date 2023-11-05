9498c7cbad96aa6167f4e34b29b135095f158777
inane-phe <127912578+inane-phe@users.noreply.github.com>
2023-11-05 22:58:44 +0800

    Add files via upload

--- PickColor/PickColor/Form1.Designer.cs
+++ PickColor/PickColor/Form1.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // start
             // 
-            this.start.Location = new System.Drawing.Point(12, 12);
+            this.start.Location = new System.Drawing.Point(6, 12);
             this.start.Name = "start";
             this.start.Size = new System.Drawing.Size(75, 23);
             this.start.TabIndex = 0;
@@ -41,22 +60,191 @@
             this.start.UseVisualStyleBackColor = true;
             this.start.Click += new System.EventHandler(this.start_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "方塊數量(n*n)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "最大色差30";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "最小色差0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "作弊是不可能作弊的 這(ry";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "得分0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 349);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "設定時間";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 418);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "剩餘時間";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 441);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 12);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "最佳成績0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "32";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(14, 148);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 30;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(19, 231);
+            this.trackBar2.Maximum = 50;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 11;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(16, 370);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 12;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 282);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "我憑本事開的掛";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(87, 12);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 14;
+            this.reset.Text = "重置";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(457, 485);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.start);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button reset;
     }
 }
 
--- PickColor/PickColor/Form1.cs
+++ PickColor/PickColor/Form1.cs
@@ -15,101 +15,295 @@ namespace PickColor
         static Random R = new Random();
         static Random G = new Random();
         static Random B = new Random();
+        static Random C = new Random();
+        static Random S = new Random();
         Random ans = new Random();
         int r = R.Next(0, 255);
         int g = G.Next(0, 255);
         int b = B.Next(0, 255);
-        int c = 50;
-
-        int m = 0;
-        int n = 2;
-        int n3 = 3;
-        int number = 2;
-
+        int c = 50,cMax=30,cMin=0;
+        int s= S.Next(0,100);
+        int m = 0, n = 2, limit = 32, sorce = 0, time = 500, maxS = 0, ;
         Button[] buttons = new Button[4];
         Button[] buttons3 = new Button[9];
         public Form1()
         {
             InitializeComponent();
-        }
-        void btn_Click(object sender, EventArgs e)
-        {
-
-        }
-        void btn_TClick(object sender, EventArgs e)
-        {
-            createBtn();
-            if (number == 3)
-            {
-                m = ans.Next(0, number);
-                if ((b + c) > 255)
-                {
-                    buttons3[m].BackColor = Color.FromArgb(r, g, b - c);
-                }
-                else
-                {
-                    buttons3[m].BackColor = Color.FromArgb(r, g, b + c);
-                }
-                buttons3[m].Click += new EventHandler(btn_TClick);
-            }
-        }
-        void createBtn()
-        {
-            r = R.Next(0, 255);
-            g = G.Next(0, 255);
-            b = B.Next(0, 255);
-            for (int i = 0; i < 4; i++)
-            {
-                this.Controls.Remove(buttons[i]);//移除                 
-            }
-            for (int i = 0; i < n3; i++)
-            {
-                for (int j = 0; j < n3; j++)
-                {
-                    buttons3[i * n3 + j] = new Button();
-                    buttons3[i * n3 + j].Name = "button" + (i * n3 + j);
-                    buttons3[i * n3 + j].Text = "";
-                    buttons3[i * n3 + j].Size = new Size(400 / n3, 400 / n3);
-                    buttons3[i * n3 + j].BackColor = Color.FromArgb(r, g, b);
-                    buttons3[i * n3 + j].Location = new Point(100 + j * (400 / n3), i * (400 / n3));
-                    buttons3[i * n3 + j].Click += new EventHandler(btn_TClick);
-                }
-            }
-            this.Controls.AddRange(buttons3);
-            c = 90;
-            number++;
-        }
+        } 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Width = 600;
-            this.Height = 500;
+            this.Width = 700;
+            this.Height = 600;
+            int needCheck = ans.Next();
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    buttons[i * n + j] = new Button();
-                    buttons[i * n + j].Name = "button" + (i * 2 + j);
-                    buttons[i * n + j].Text = "";
-                    buttons[i * n + j].Size = new Size(400 / n, 400 / n);
-                    buttons[i * n + j].BackColor = Color.YellowGreen;
-                    buttons[i * n + j].Location = new Point(100 + j * (400 / n), i * (400 / n));
-                    buttons[i * n + j].Click += new EventHandler(btn_Click);
+                    buttons[i * n + j] = creatButton(i, j);
                 }
             }
             this.Controls.AddRange(buttons);
         }
-        private void start_Click(object sender, EventArgs e)
+        private Button creatButton(int i, int j)
         {
-            m = ans.Next(0, number);
-            if ((b+c)>255)
+            Button temp = new Button();
+            temp.Name = "button" + (i * 2 + j);
+            temp.Text = "";
+            temp.ForeColor = Color.FromArgb(Math.Abs(r-255),g, b); ;
+            temp.Size = new Size(400 / n, 400 / n);
+            if (s >= 33 && s <= 66)
             {
-                buttons[m].BackColor = Color.FromArgb(r, g, b - c);
+                r -= r / 3;
+                temp.BackColor = Color.FromArgb(r, g, b);
+     
+            }
+            else if (s > 66)
+            {
+                g -= g / 3;
+                temp.BackColor = Color.FromArgb(r, g, b);
             }
             else
             {
-                buttons[m].BackColor = Color.FromArgb(r, g, b + c);
+                b -= b / 3;
+                temp.BackColor = Color.FromArgb(r, g, b);
             }
-            buttons[m].Click += new EventHandler(btn_TClick);
+            
+            temp.Location = new Point(250 + j * (400 / n), i * (400 / n));
+            temp.Click += new EventHandler(btn_Click);
+            return temp;
+        }
+        void btn_Click(object sender, EventArgs e)
+        {
+            if (sender.Equals(buttons[m]))
+            {
+                removeButton();
+                if (n < limit)
+                {
+                    n += 1;
+                }
+                sorce += 1;
+                time  += 1;
+                label5.Text = "目前得分" + sorce;
+                buttons = new Button[n * n];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        buttons[i * n + j] = creatButton(i, j);
+                        if (checkBox1.Checked)
+                        {
+                            buttons[i * n + j].Text = i + "-" + j;
+                        }
+                    }
+
+                }
+                start_Click(sender, e);
+                this.Controls.AddRange(buttons);
+            }
+            else
+            {
+                sorce -= 1;
+                time -= 1;
+                label5.Text = "目前得分" + sorce;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time > 0 && time < 500)
+            {
+                time -= 1;
+                label7.Text = "剩餘時間為" + time / 60 + "分" + time % 60 + "秒";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("計時結束囉，得分為" + sorce);
+                if (sorce > maxS)
+                {
+                    maxS = sorce;
+                    label8.Text = "最佳分數為" + maxS;
+                }
+                reset_Click(sender, e);
+            }
+        }
+        private void removeButton()
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                this.Controls.Remove(buttons[i]);
+            }
+            if (s >= 33 && s <=66)
+            {
+                r = R.Next(0, 255);
+                g = G.Next(0, 255);
+                b = B.Next(0, 255);
+                r -= r/3;
+            }
+            else if (s>66)
+            {
+                r = R.Next(0, 255);
+                g = G.Next(0, 255);
+                b = B.Next(0, 255);
+                g -= g/3;
+            }
+            else
+            {
+                r = R.Next(0, 255);
+                g = G.Next(0, 255);
+                b = B.Next(0, 255);
+                b -= b/3;
+            }
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (time < 500 && time > 0)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                label6.Text = "不計時";
+            }
+            m = ans.Next(0, n * n);
+            start.Enabled = false;
+            reset.Enabled = true;
+            c = C.Next(cMin, cMax);
+            if ((b + c) > 255 || s<c)
+            {
+                if (n % 3 == 0)
+                    buttons[m].BackColor = Color.FromArgb(r - c, g, b);
+                else if (n % 3 == 1)
+                    buttons[m].BackColor = Color.FromArgb(r, g - c, b);
+                else
+                    buttons[m].BackColor = Color.FromArgb(r, g, b - c);
+            }
+            else
+            {
+                if (n % 3 == 0)
+                    buttons[m].BackColor = Color.FromArgb(r + c, g, b);
+                else if (n % 3 == 1)
+                    buttons[m].BackColor = Color.FromArgb(r, g + c, b);
+                else
+                    buttons[m].BackColor = Color.FromArgb(r, g, b + c);
+            }
+            if (checkBox1.Checked)
+                label4.Text = "目前答案" + m / n + "-" + m % n + "色差為" + c;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            cMax = trackBar1.Value;
+            if (cMax == 0)
+            {
+                MessageBox.Show("打咩٩(๑`^´๑)۶");
+            }
+            else
+            {
+                if (cMax <= cMin)
+                {
+                    cMin = cMax - 1;
+                    trackBar2.Value = cMin;
+                    label3.Text = "最小色差" + cMin.ToString();
+                }
+                label2.Text = "最大色差" + cMax.ToString();
+            }
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            cMin = trackBar2.Value;
+            if (cMin == 50)
+            {
+                MessageBox.Show("打咩٩(๑`^´๑)۶");
+            }
+            else
+            {
+                if (cMin >= cMax)
+                {
+                    cMax = cMin + 1;
+                    trackBar1.Value = cMax;
+                    label2.Text = "最大色差" + cMax.ToString();
+                }
+                label3.Text = "最小色差" + cMin.ToString();
+            }
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            time = trackBar3.Value * 5;
+            if (time == 100 || time == 0)
+            {
+                label6.Text = "不計時";
+            }
+            else
+            {
+                label6.Text = "倒數計時設定為" + time / 60 + "分" + time % 60 + "秒";
+                label7.Text = "剩餘時間為" + time / 60 + "分" + time % 60 + "秒";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                for (int i = 0; i < n * n; i++)
+                {
+                    buttons[i].ForeColor = Color.White;
+                    buttons[i].Text = i / n + "-" + i % n;
+
+                }
+                label4.Text = "目前答案" + m / n + "-" + m % n +"色差為"+c;
+            }
+            else
+            {
+                for (int i = 0; i < n * n; i++)
+                {
+                    buttons[i].Text = "";
+                    buttons[i].ForeColor = Color.White;
+                }
+                label4.Text = "作弊是不可能作弊的 這輩子都(ry";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0)
+                limit = Int32.Parse(textBox1.Text);
+        }
+        private void reset_Click(object sender, EventArgs e)
+        {
+            start.Enabled = true;
+            reset.Enabled = false;
+            timer1.Enabled = false;
+            removeButton();
+            n = 2;
+            sorce = 0;
+            time = trackBar2.Value * 5;
+            if (time == 500 || time == 0)
+            {
+                label6.Text = "不計時";
+            }
+            else
+            {
+                label6.Text = "倒數計時設定為" + time / 60 + "分" + time % 60 + "秒";
+                label7.Text = "剩餘時間為" + time / 60 + "分" + time % 60 + "秒";
+            }
+            buttons = new Button[n * n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    buttons[i * n + j] = creatButton(i, j);
+                    if (checkBox1.Checked)
+                    {
+                        buttons[i * n + j].ForeColor = Color.White;
+                        buttons[i * n + j].Text = i + "-" + j;
+                    }
+                }
+
+            }
+            this.Controls.AddRange(buttons);
         }
     }
 
